c3786b0279afac53573b927c57ba24a414c1d734
Fliiiiiou <cahoreau.florian@hotmail.fr>
2017-02-08 03:46:18 +0100

    Adding of the basic files

---
+++ Exemple de timer avec la commande after.py
@@ -0,0 +1,31 @@
+﻿import time
+from tkinter import Label, StringVar, Tk, font
+
+ncycles = 1000
+ndigits = len(str(ncycles)) - 1
+timeout = 10 # milliseconds
+root = Tk()
+font.nametofont('TkDefaultFont').configure(size=10) # big
+text = StringVar()
+text.set('0'*ndigits)
+Label(root, textvariable=text).pack()
+
+# center the window
+root.update_idletasks() # necessary to update w, h
+w, h = root.winfo_width(), root.winfo_height()
+x = root.winfo_screenwidth() // 2  - w // 2
+y = root.winfo_screenheight() // 2 - h // 2
+root.geometry('{}x{}+{}+{}'.format(w, h, x, y))
+
+def timer():
+    return round(time.perf_counter() * 1000)
+
+def update_timer(i=0):
+    if i >= ncycles:
+        root.destroy() # exit
+    else:
+        root.after(timeout - timer() % timeout, update_timer, i+1)
+        text.set('{:0{width}d}'.format(i, width=ndigits))
+
+root.after(timeout - timer() % timeout, update_timer) # start timer
+root.mainloop()
---
+++ Homebrew Mailbomber.py
@@ -0,0 +1,82 @@
+﻿from tkinter import *
+import time #Necessaire pour le timer
+
+#DébutTimer
+class App():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.label = tk.Label(text="")
+        self.label.pack()
+        self.update_clock()
+        self.root.mainloop()
+
+    def update_clock(self):
+        now = time.strftime("%H:%M:%S")
+        self.label.configure(text=now)
+        self.root.after(1000, self.update_clock)
+#FinTimer
+
+class Fonctions_Boutons() :
+    def Start() :
+        ab#Insérer fonction
+    def Stop() :
+        ab#Insérer fonction
+    def Exit():
+        ab#Insérer fonction
+
+fen=Tk()
+fen.title("MailBomber")
+fen.geometry('800x600')#Taille de la fenêtre en pixel
+
+#Boutons :
+bouton_Start = Button(fen, text="Demarrer",command=Start) #Bouton Démarrer
+bouton_Start.pack(pady=10)
+
+bouton_Stop = Button(fen, text="Arrêter",command=Stop) #Bouton Arrêter
+bouton_Stop.pack(pady=10)
+
+bouton_Exit = Button(fen, text="Quitter",command=Exit) #Bouton Quitter
+bouton_Exit.pack(pady=10)
+#Fin de boutons.
+
+#Zones de texte :
+var_texte = StringVar()
+ligne_texte = Entry(fen, textvariable=var_texte, width=30)
+ligne_texte.pack(pady=10)
+
+var_texte2 = StringVar()
+ligne_texte = Entry(fen, textvariable=var_texte2, width=30)
+ligne_texte.pack(pady=10)
+
+var_texte3 = StringVar()
+ligne_texte = Entry(fen, textvariable=var_texte3, width=30)
+ligne_texte.pack(pady=10)
+
+var_texte4 = StringVar()
+ligne_texte = Entry(fen, textvariable=var_texte4, width=30)
+ligne_texte.pack(pady=10)
+
+var_texte5 = StringVar()
+ligne_texte = Entry(fen, textvariable=var_texte5, width=30)
+ligne_texte.pack(pady=10)
+#Fin de zones de texte.
+
+#Début des paramètres d'affichages graphique
+
+#Fin des paramètres d'affichages graphique
+
+#Step4 :
+
+#Dim MyMailssage As New MailMessage()
+#MyMailssage.From = New MailAddress(textbox1.Text)
+#MyMailssage.To.Add(textbox3.Text)
+#MyMailssage.Subject = (textbox4.Text)
+#MyMailssage.Body = textbox5.Text
+#Dim SMTPServer As New SmtpClient("smtp.gmail.com")
+#SMTPServer.Port = 587
+#SMTPServer.Credentials = New System.Net.NetworkCredential(textbox1.Text, textbox2.Text)
+#SMTPServer.EnableSsl = True
+#Label1.Text = Val(Label1.Text + 1)
+#SMTPServer.Send(MyMailssage)
+
+fen.mainloop()
